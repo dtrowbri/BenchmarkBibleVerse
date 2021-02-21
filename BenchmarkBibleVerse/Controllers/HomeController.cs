fd90b6c2db47686675693995d020146137f31e37
Donald_Trowbridge <dtrowbridg@my.gcu.edu>
2021-02-20 23:21:28 -0700

    Adding logger to the home controller

--- BenchmarkBibleVerse/Controllers/HomeController.cs
+++ BenchmarkBibleVerse/Controllers/HomeController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BenchmarkBibleVerse.Service.Utility;
 
 namespace BenchmarkBibleVerse.Controllers
 {
     public class HomeController : Controller
     {
+        private ILogger logger;
+        
+        public HomeController(ILogger logger)
+        {
+            this.logger = logger;
+        }
         // GET: Home
         public ActionResult Index()
         {
+            logger.Info("Action Index in Home controller has been invoked.");
             return View("Home");
         }
     }
